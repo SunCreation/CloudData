a6c337ba47bdcb4fdc6257ef736c2408ee61cbfa
SunCreation <cake0103@nate.com>
2022-02-10 03:02:03 +0000

    hchang

--- math/data/test.py
+++ math/data/test.py
@@ -1,9 +1,16 @@
 import sys
 import pandas as pd
 import time
-data = pd.read_csv("agutrain.csv")
+# data = pd.read_csv("agutrain.csv")
+# data = pd.read_csv("Valtrain.csv")
 
-data['problem'] = data['problem'].apply(lambda x: x + '<sys>') + data['class'].apply(lambda x: str(x) + '<sys>')
 
-print(data)
-data.to_csv("Agutrain.csv")
+# data['problem'] = data['problem'].apply(lambda x: x + '<sys>') + data['class'].apply(lambda x: str(x) + '<sys>')
+
+# print(data)
+# data.to_csv("Valtrain.csv")
+# data.to_csv("Agutrain.csv")
+
+
+print(ord('가'), ord('힣'), ord(' '))
+print(chr(44032), chr(32), chr(55203), sep='')
